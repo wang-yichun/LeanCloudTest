e610c57b730bb55a1cb2dcfbff740a0491981d4e
Ethan@MECHREV <prgethan@126.com>
2018-01-11 10:22:50 +0800

    upd getone

--- cloud.js
+++ cloud.js
@@ -7,3 +7,13 @@ AV.Cloud.define('hello', function(request) {
     console.log(request.params);
     return 'Hello world!';
 });
+
+AV.Cloud.define('get_one', function(request) {
+    console.log(request.params);
+    var query = new AV.query('Character');
+    query.get('5a54602067f3560062e6de00').then(function (item) {
+        return item;
+    }, function (error) {
+        console.error(error.message)
+    });
+});
