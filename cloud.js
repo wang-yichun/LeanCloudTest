aeecae6cb07066b24ae360d742bc458f7f707c8c
Ethan@MECHREV <prgethan@126.com>
2018-01-11 10:46:28 +0800

    upd

--- cloud.js
+++ cloud.js
@@ -10,12 +10,24 @@ AV.Cloud.define('hello', function(request) {
     return 'Hello world!';
 });
 
-AV.Cloud.define('get_one', function(request) {
+AV.Cloud.define('get_one_json', function(request) {
     console.log(request.params);
     var query = Character.query;
     query.get(request.params.oid).then(function (item) {
-        console.log(item)
-        return item;
+        console.log(item);
+        return item._hashedJSON;
+    }, function (error) {
+        console.error(error.message)
+    });
+});
+
+
+AV.Cloud.define('get_one_dic', function(request) {
+    console.log(request.params);
+    var query = Character.query;
+    query.get(request.params.oid).then(function (item) {
+        console.log(item);
+        return item.attributes;
     }, function (error) {
         console.error(error.message)
     });
