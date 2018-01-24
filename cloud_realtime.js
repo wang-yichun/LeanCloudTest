0eeeab9a1f49aa15ac144bee341d57ee9bf0e11a
Ethan@MECHREV <prgethan@126.com>
2018-01-24 14:11:40 +0800

    upd

--- cloud_realtime.js
+++ cloud_realtime.js
@@ -5,16 +5,35 @@ var Character = AV.Object.extend('Character');
 /**
  * 一个简单的云代码方法
  */
-AV.Cloud.define('get_conversation_online_count', function(request) {
+AV.Cloud.define('get_conversation_online_count', function(request, response) {
     console.log(request.params);
 
-    var cid = 'c007';
-    var query = AV.Conversation.query.equalTo('name', cid);
-    query.find().then(function (conversations) {
-        for (var i = 0; i < conversations.length; i++) {
-            var conversation = conversations[i];
-            console.log(conversation.id);
+    var cids = request.params['club_ids'];
+    var c = 0;
+    var finishCheck = function () {
+        c++;
+        if (c === cids.length) {
+            response.success(cids);
         }
-    }).catch(console.error.bind(console));
+    };
 
+    var result = {};
+
+    for (var i = 0; i < cids.length; i++) {
+        var cid = cids[i];
+        var query = AV.Conversation.query.equalTo('name', cid);
+        query.find().then(function (conversations) {
+
+            if (conversations.length === 1) {
+                var conversation = conversations[0];
+                conversation.count().then(function (count) {
+                    result[cid] = {count: count, cid: conversation.id};
+                    finishCheck();
+                });
+            } else {
+                result[cid] = {count: 0}
+                finishCheck();
+            }
+        }).catch(console.error.bind(console));
+    }
 });
